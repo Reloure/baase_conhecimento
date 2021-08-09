91f045dd76fc10d1c503fe27b3d0a9172bff751f
fitasmcm <everton@fitasmcm.com.br>
2021-08-09 12:36:13 -0300

    atualizacao

---
+++ listar_tabelas_banco.sql
@@ -0,0 +1,22 @@
+select owner, table_name from all_tables
+where owner = 'ASUPD';
+
+/
+
+SELECT * FROM ASUPD.DDTC -- -- descricao das colunas das tabelas do as3
+WHERE TABELA = 'MPALMO'
+
+/
+
+SELECT * FROM ASUPD.DDT -- descricao das tabelas do as3
+WHERE TABELA = 'MPALMO'
+
+--RH
+--NFE
+--MCM
+--ASUPD
+--ASPRINC
+--ASDATA
+--ASAUDIT
+
+
